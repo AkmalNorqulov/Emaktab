2bca0681b927426ed0bcd3ba1a33b676dda09290
Akmal <akmal9999007@gmail.com>
2025-09-18 20:32:39 +0500

    hh

--- wsgi.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-from app import app  # replace with your Flask app instance
-
-# Add your project path so Python can find it
-sys.path.insert(0, os.path.dirname(__file__))
-
-application = app  # WSGI expects "application"
